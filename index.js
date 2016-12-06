09b6b03b4caa36afb9138e9b29d1b39397f1a8ff
Shane F. Carr <shane.carr@wustl.edu>
2016-12-06 00:05:21 -0800

    Assorted bug fixes.  Adding default content for new files.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var CardRenderer = require("./lib/render");
 var CcsbReader = require("./lib/ccsb");
 var csv = require("./lib/csv");
+var defaults = require("./lib/defaults");
 var OptionsParser = require("./lib/options");
+var PageRenderer = require("./lib/page");
 var ReadAndRender = require("./lib/read-and-render");
 
 module.exports = {
 	CardRenderer,
 	CcsbReader,
 	csv,
+	defaults,
 	OptionsParser,
+	PageRenderer,
 	ReadAndRender
 };
